e3f47f722f088902ea15d54b683215e6b5416213
Red-Roger <144430361+Red-Roger@users.noreply.github.com>
2023-10-06 17:30:01 +0300

    ///

--- SortOut/sort.py
+++ SortOut/sort.py
@@ -1,5 +1,7 @@
 import sys
 import shutil
+import re
+import os
 from pathlib import Path
 for arg in sys.argv:
     path = arg
@@ -12,53 +14,62 @@ docs = ("doc", "docx", "txt", "pdf", "xlsx", "pptx")
 music = ("mp3", "ogg", "wav", "amr")
 archives = ("zip", "rar", "tar", "gz")
 
-def makedirs(root_path, dir_path):
-        new_dir_path = root_path + dir_path
-        p_newdir = Path (new_dir_path)
-        p_newdir.mkdir(exist_ok=True)
-
 def copy_file (root_path, source_path, img_path):
     dest_path = ROOT + img_path
     source_path = root_path + "/" + source_path
     p_file = Path (source_path)
     if p_file.is_file():
-        shutil.move(source_path,dest_path)
+            shutil.move(source_path,dest_path)
     else:
         root_path = source_path
         findfiles (root_path)
 
+def makedirs(root_path, dir_path):
+        new_dir_path = root_path + dir_path
+        p_newdir = Path (new_dir_path)
+        p_newdir.mkdir(exist_ok=True)
+
+def normalize(name):
+    name = re.sub(r"[^+\w[.]", "_",name)
+    return name
+
 def findfiles (root_path):
     p = Path (root_path)
     for i in p.iterdir():
-        if i.suffix[1:].lower() in (images):
-            img_path = "/Зображення"
+        name = normalize(i.name)
+        old_name = root_path + "/" + i.name
+        new_name = root_path + "/" + name
+        os.rename(old_name, new_name)
+        suffix = i.suffix
+        if suffix[1:].lower() in (images):
+            img_path = "/images"
             makedirs(ROOT, img_path)
-            copy_file (root_path, i.name, img_path)
+            copy_file (root_path, name, img_path)
 
         elif i.suffix[1:].lower() in (video):
-            img_path = "/Відео"
+            img_path = "/video"
             makedirs(ROOT, img_path)
-            copy_file (root_path, i.name, img_path)
+            copy_file (root_path, name, img_path)
 
         elif i.suffix[1:].lower() in (docs):
-            img_path = "/Документи"
+            img_path = "/documents"
             makedirs(ROOT, img_path)
-            copy_file (root_path, i.name, img_path)
+            copy_file (root_path, name, img_path)
 
         elif i.suffix[1:].lower() in (music):
-            img_path = "/Музика"
+            img_path = "/audio"
             makedirs(ROOT, img_path)
-            copy_file (root_path, i.name, img_path)
+            copy_file (root_path, name, img_path)
 
         elif i.suffix[1:].lower() in (archives):
-            img_path = "/Архіви"
+            img_path = "/archives"
             makedirs(ROOT, img_path)
-            copy_file (root_path, i.name, img_path)
+            copy_file (root_path, name, img_path)
  
         else:
-            img_path = "/Інше"
+            img_path = "/other"
             makedirs(ROOT, img_path)
-            copy_file (root_path, i.name, img_path)
+            copy_file (root_path, name, img_path)
 
 p = Path (path)
 if p.is_dir():
