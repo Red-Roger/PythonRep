514cba44abe42a907c8085ce3cbcd8a592fab6c3
Red-Roger <144430361+Red-Roger@users.noreply.github.com>
2023-10-15 10:51:17 +0300

    \\\

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
-setup(name='clean_folder',
+setup (name='clean_folder',
       version='1',
       description='clean folder code',
       url='https://github.com/Red-Roger/PythonRep/tree/main/clean_folder',
       author='Red Roger',
       author_email='postny@email.ua',
       license='MIT',
-      packages=['clean_folder'])
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main']}
+)
