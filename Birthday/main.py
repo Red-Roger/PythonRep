366e9499e3a7857b274c8b25b0e0dd7552c0ac40
Red-Roger <144430361+Red-Roger@users.noreply.github.com>
2023-10-21 14:38:46 +0300

    ///

--- Birthday/main.py
+++ Birthday/main.py
@@ -2,8 +2,29 @@ from datetime import date, datetime
 
 
 def get_birthdays_per_week(users):
-    users ={}
-    return users
+    users_birth = {}
+    if users == []:
+        return users_birth
+    today = date.today()
+    WEEKNAMES = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday", 5:"Saturday", 6:"Sunday"}
+    for value in users:
+        user_birth = value ["birthday"]
+        if today.month == 12 and today.day > 24 and user_birth.month == 1:
+            user_birth_norm = user_birth.replace (year = today.year + 1)
+        else:
+            user_birth_norm = user_birth.replace (year = today.year)
+        difference = user_birth_norm - today
+        if difference.days >= 0 and difference.days < 8:
+            index = user_birth_norm.weekday()
+            if index == 5 or index == 6:
+                index = 0
+            user_birth_name = WEEKNAMES[index]
+            if users_birth.get(user_birth_name):
+                users_birth [WEEKNAMES[index]].append (value["name"])
+            else:
+                users_birth [WEEKNAMES[index]] = [value["name"]]
+
+    return users_birth
 
 
 if __name__ == "__main__":
@@ -17,7 +38,3 @@ if __name__ == "__main__":
     for day_name, names in result.items():
         print(f"{day_name}: {', '.join(names)}")
 
-users = [{"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()},
-         {"name": "John Smith", "birthday": datetime(1956, 5, 2).date()},
-         {"name": "Steven Spilberg", "birthday": datetime(1954, 9, 11).date()},
-         {"name": "Vasia Pupkin", "birthday": datetime(1966, 3, 3).date()}]
