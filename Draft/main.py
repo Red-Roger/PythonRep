ec25057296d8bb2b279e5157c9d78d805d87f44b
Red-Roger <144430361+Red-Roger@users.noreply.github.com>
2023-11-04 09:38:48 +0200

    ///

--- Draft/main.py
+++ Draft/main.py
@@ -1,11 +1,37 @@
-from functools import reduce
+class Contacts:
+    current_id = 1
 
+    def __init__(self):
+        self.contacts = []
 
-def amount_payment(payment):
-    cleared = []
-    for i in filter(lambda x: x > 0, payment):
-        cleared.append(i)
-    return reduce ((lambda x, y: x + y), cleared)
+    def list_contacts(self):
+        return self.contacts
 
-payment = [1, -3, 4]
-print (amount_payment(payment))
+    def add_contacts(self, name, phone, email, favorite):
+        self.contacts.append(
+            {
+                "id": Contacts.current_id,
+                "name": name,
+                "phone": phone,
+                "email": email,
+                "favorite": favorite,
+            }
+        )
+        Contacts.current_id += 1
+
+    def get_contact_by_id(self, id):
+        for value in self.contacts:
+            if value["id"] == id:
+                return value
+        return None
+
+    def remove_contacts(self, id):
+        for value in self.contacts:
+            if value["id"] == id:
+                index = self.contacts.index(value)
+                self.contacts.pop(index)
+
+con = Contacts()
+con.add_contacts("Tolia","+380505056980","email",True)
+print (con.get_contact_by_id (1))
+
