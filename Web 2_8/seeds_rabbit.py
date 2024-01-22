e70e059a88fa1a87743403b51224ea96113806b8
Red-Roger <144430361+Red-Roger@users.noreply.github.com>
2024-01-22 17:06:33 +0200

    ///

---
+++ Web 2_8/seeds_rabbit.py
@@ -0,0 +1,8 @@
+from models_rabbit import Contacts
+import connect
+
+
+Contacts (fullname = "Adam Smith", email = "smith@gmail.com", issent = False).save()
+Contacts (fullname = "John Lock", email = "lock@gmail.com", issent = False).save()
+Contacts (fullname = "Tomas Gobbs", email = "gobbs@gmail.com", issent = False).save()
+Contacts (fullname = "Mary Adler", email = "adler@gmail.com", issent = False).save()
