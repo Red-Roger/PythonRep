5cb03ad10a068fbdd2d046d1272e4c00705ee7bc
Red-Roger <144430361+Red-Roger@users.noreply.github.com>
2024-02-17 08:04:08 +0200

    ///

--- Web_2_10_1/notes/users/views.py
+++ Web_2_10_1/notes/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-
-from .forms import RegisterForm
-
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import messages
 
 def signupuser(request):
     if request.user.is_authenticated:
@@ -17,3 +18,22 @@ def signupuser(request):
 
     return render(request, 'users/signup.html', context={"form": RegisterForm()})
 
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='noteapp:main')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='noteapp:main')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='noteapp:main')
